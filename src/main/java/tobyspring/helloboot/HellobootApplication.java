bda1c5a548d1be1ec34424e6fbcb0968550d86c2
le2sky <leehaneul990623@gmail.com>
2023-06-17 03:54:29 +0900

    chore : initial commit

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,8 @@
+package tobyspring.helloboot;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello containerless standalone application");
+	}
+}
