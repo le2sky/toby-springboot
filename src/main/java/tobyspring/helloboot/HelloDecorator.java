f804a17241ac68fcb98742bfc0bea2680d9e745a
le2sky <leehaneul990623@gmail.com>
2023-06-20 23:58:20 +0900

    feat : 데코레이터 생성

---
+++ src/main/java/tobyspring/helloboot/HelloDecorator.java
@@ -0,0 +1,18 @@
+package tobyspring.helloboot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloDecorator implements HelloService {
+
+    private final HelloService helloService;
+
+    public HelloDecorator(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        return helloService.sayHello(name) + "*";
+    }
+}
