419837c6b7c764ca0c4d52b22a63bce6cb22dcb0
le2sky <leehaneul990623@gmail.com>
2023-06-24 01:31:41 +0900

    feat : 자동 구성에 Environment 프로퍼티 적용

--- src/main/java/tobyspring/helloboot/WebServerConfiguration.java
+++ src/main/java/tobyspring/helloboot/WebServerConfiguration.java
@@ -4,14 +4,16 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
 
     @Bean
-    ServletWebServerFactory customerWebServerFactory() {
+    ServletWebServerFactory customerWebServerFactory(Environment env) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setPort(9090);
+        serverFactory.setPort(8080);
+        serverFactory.setContextPath(env.getProperty("contextPath"));
         return serverFactory;
     }
 }
